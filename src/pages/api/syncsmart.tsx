33e9a616478671683b1a54d146606dbe07b73420
Faris Alsharayri <farissharayri@gmail.com>
2025-03-24 03:14:02 -0400

    styles contacts page, fixes get request to search according to createdate

--- src/pages/api/syncsmart.tsx
+++ src/pages/api/syncsmart.tsx
@@ -2,8 +2,8 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 // linting error ? not anymore . . .
 interface Contact {
-  firstName: string;
-  lastName: string;
+  firstname: string;
+  lastname: string;
   email: string;
 }
 interface RequestBody {
@@ -13,21 +13,21 @@ interface RequestBody {
 
 export default async function handler( req: NextApiRequest, res: NextApiResponse) {
   const url = 'https://api.hubapi.com/crm/v3/objects/contacts'
-  const SyncSmartToken = process.env.SYNCSMART_TOKEN
+  const SyncSmartToken = process.env.HUBSPOT_SYNCSMART_TOKEN
   if (!SyncSmartToken) {
     res.status(500).json({ error: 'SyncSmart Token not found' })
     return
   }
   if (req.method === 'POST') {
     // Process a POST request
-    const { contacts } = req.body as RequestBody;
-    if (!contacts) {
-      res.status(400).json({ error: 'Contacts not found in request body' })
+    const { contactsList } = req.body as { contactsList: Contact[] };
+    if (!contactsList) {
+      res.status(400).json({ error: 'ContactsList not found in request body' })
       return
     }
     try{
     const SyncSmartList = await Promise.all(
-      contacts.map(contact => 
+      contactsList.map(contact => 
         fetch(url, {
           method: 'POST',
           headers: {
@@ -36,11 +36,11 @@ export default async function handler( req: NextApiRequest, res: NextApiResponse
           },
           body: JSON.stringify({
             properties: {
-              firstname: contact.firstName,
-              lastname: contact.lastName,
+              firstname: contact.firstname,
+              lastname: contact.lastname,
               email: contact.email,
             },
-          }),
+          })
         })
       )
     )
@@ -57,20 +57,31 @@ export default async function handler( req: NextApiRequest, res: NextApiResponse
     // Process a GET request
     try {
       // simple solution to a problem with the data duplication, fetching the most recently created 100 contacts is more accurate because those are the contacts that were generated from a fake data library and uploaded to SyncSmart
-      const response = await fetch(url + '?limit=100&sort=-createdate', {
-        method: 'GET',
+      const response = await fetch(url + '/search', {
+        method: 'POST',
         headers: {
           Authorization: `Bearer ${SyncSmartToken}`,
           'Content-Type': 'application/json',
         },
+        body:JSON.stringify({
+          limit:100,
+          sorts: [
+            {
+              propertyName: 'createdate',
+              direction: 'DESCENDING',
+            },
+          ],
+        }),
       });
+      
       if (!response.ok) {
         throw new Error(`Error fetching contacts: ${response.statusText}`);
       }
       const data = await response.json() as { results: Contact[] };
       
       console.log("Latest 100 contacts:", data.results.slice(0,5));
-      return data.results;
+      res.status(200).json(data.results); 
+      // return data.results;
     } catch (error) {
       console.error("Error fetching contacts:", error);
     }
--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -1,8 +1,7 @@
-
 import React from 'react'
-import {faker} from '@faker-js/faker';
-import {Button, Table, TableCell} from '@mui/material';
-import { useState,useEffect } from 'react';
+import { faker } from '@faker-js/faker'
+import { Button, Table, TableCell } from '@mui/material'
+import { useState } from 'react'
 
 interface Contact {
   firstName: string;
@@ -11,27 +10,26 @@ interface Contact {
 }
 
 export default function Contacts() {
-  const [message, setMessage] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("")
+  const [loading, setLoading] = useState(false)
   const [contactsList, setContactsList] = useState([])
   const [SyncSmartList, setSyncSmartList] = useState([])
   const [LyntonList, setLyntonList] = useState([])
+
   const handleGenerateFakeData = () => {
-    setMessage("Generating fake data...");
-    setLoading(true);
-    const contacts: Array<object> = [];
-    let firstName;
-    let lastName;
-    let email;
+    setMessage("Generating fake data...")
+    setLoading(true)
+    const contacts: Array<object> = []
+    let firstName, lastName, email
     for (let i = 0; i < 100; i++) {
-      firstName = (faker.person.firstName as () => string)();
-      lastName = (faker.person.lastName as () => string)();
-      email = (faker.internet.email as () => string)();
+      firstName = (faker.person.firstName as () => string)()
+      lastName = (faker.person.lastName as () => string)()
+      email = (faker.internet.email as () => string)()
       const contact = {
         firstname: firstName,
         lastname: lastName,
         email: email,
-      };
+      }
       contacts.push(contact)
     }
     setContactsList(contacts)
@@ -40,110 +38,113 @@ export default function Contacts() {
   }
 
   const handleUploadtoSyncSmart = async () => {
-    setMessage("Uploading contacts to SyncSmart...");
-    setLoading(true);
+    setMessage("Uploading contacts to SyncSmart...")
+    setLoading(true)
     try {
       const res = await fetch('/api/syncsmart', {
         method: 'POST',
-        headers: {'Content-Type': 'application/json',},
-        body: JSON.stringify({  contactsList }),
-      });
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ contactsList }),
+      })
       if (res.ok) {
-        setMessage("Contacts uploaded to SyncSmart successfully.");
+        setMessage("Contacts uploaded to SyncSmart successfully.")
       } else {
-        setMessage("Unable to upload contacts to SyncSmart endpoint.");
+        setMessage("Unable to upload contacts to SyncSmart endpoint.")
       }
     } catch (error) {
-      setMessage("Unable to complete POST Request (SyncSmart): " + (error as string));
+      setMessage("Unable to complete POST Request (SyncSmart): " + (error as string))
       console.error('Request Error:', error)
     } finally {
-      setLoading(false);
+      setLoading(false)
     }
   }
+
   const handlefetchSyncSmart = async () => {
-    setMessage("Fetching contacts from SyncSmart...");
-    setLoading(true);
+    setMessage("Fetching contacts from SyncSmart...")
+    setLoading(true)
     try {
       const res = await fetch('/api/syncsmart', {
         method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+        headers: { 'Content-Type': 'application/json' },
+      })
       if (res.ok) {
-        const data = await res.json() as Contact[] ;
-        setSyncSmartList(data as []);
-        setMessage("Contacts fetched from SyncSmart successfully.");
+        const data = await res.json() as Contact[]
+        setSyncSmartList(data as [])
+        setMessage("Contacts fetched from SyncSmart successfully.")
       } else {
-        setMessage("Unable to fetch contacts from SyncSmart endpoint.");
+        setMessage("Unable to fetch contacts from SyncSmart endpoint.")
       }
     } catch (error) {
-      setMessage("Unable to complete fetch Request (SyncSmart):" + (error as string));
+      setMessage("Unable to complete fetch Request (SyncSmart):" + (error as string))
       console.error('Request Error:', error)
     } finally {
-      setLoading(false);
+      setLoading(false)
     }
   }
+
   return (
-    <div >
-      {/* -------------- generating contacts field -------------- */}
-          <p>{loading? "loading...": ""}</p>
-          <p>{message}</p>
-        <div>
-          <h1>Generate Fake Contacts</h1> 
-          <Button onClick={handleGenerateFakeData}>generate</Button>
-          
+    <div>
+      <p>{loading ? "loading..." : ""}</p>
+      <p>{message}</p>
+      {/* Flex container for the three sections */}
+      <div style={{ display: 'flex', flexDirection: 'row', gap: '1rem' }}>
+        {/* Generate Fake Contacts */}
+        <div style={{ flex: 1, border: '1px solid #ccc', padding: '1rem' }}>
+          <h1>Generate Fake Contacts</h1>
+          <Button onClick={handleGenerateFakeData}>Generate</Button>
           {contactsList[0] && 
             <Table>
               <thead>
-                  <tr>
-                    <th>First Name</th>   
-                    <th>Last Name</th>
-                    <th>Email</th>
-                  </tr>
-              </thead>
-              {contactsList.map((contact,index)=>(
-                <tr key={index}>
-                  <TableCell>{contact.firstname} </TableCell>
-                  <TableCell>{contact.lastname} </TableCell>
-                  <TableCell>{contact.email} </TableCell>
+                <tr>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Email</th>
                 </tr>
-              ))}
+              </thead>
+              <tbody>
+                {contactsList.map((contact, index) => (
+                  <tr key={index}>
+                    <TableCell>{contact.firstname}</TableCell>
+                    <TableCell>{contact.lastname}</TableCell>
+                    <TableCell>{contact.email}</TableCell>
+                  </tr>
+                ))}
+              </tbody>
             </Table>
           }
         </div>
-        {/* -------------- SyncSmart field -------------- */}
-        <div>
+        {/* SyncSmart */}
+        <div style={{ flex: 1, border: '1px solid #ccc', padding: '1rem' }}>
           <h1>SyncSmart</h1>
-          
-          <Button onClick={handleUploadtoSyncSmart}>Upload to SyncSmart
-          </Button>
+          <Button onClick={handleUploadtoSyncSmart}>Upload to SyncSmart</Button>
+          <Button onClick={handlefetchSyncSmart}>Fetch contacts from SyncSmart</Button>
           {SyncSmartList[0] && 
             <Table>
               <thead>
-                  <tr>
-                    <th>First Name</th>   
-                    <th>Last Name</th>
-                    <th>Email</th>
-                  </tr>
-              </thead>
-              {SyncSmartList.map((contact,index)=>(
-                <tr key={index}>
-                  <TableCell>{contact.firstname} </TableCell>
-                  <TableCell>{contact.lastname} </TableCell>
-                  <TableCell>{contact.email} </TableCell>
+                <tr>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Email</th>
                 </tr>
-              ))}
+              </thead>
+              <tbody>
+                {SyncSmartList.map((contact, index) => (
+                  <tr key={index}>
+                    <TableCell>{contact.properties.firstname}</TableCell>
+                    <TableCell>{contact.properties.lastname}</TableCell>
+                    <TableCell>{contact.properties.email}</TableCell>
+                  </tr>
+                ))}
+              </tbody>
             </Table>
           }
         </div>
-        
-        {/* -------------- Lynton field -------------- */}
-        <div>
+        {/* Lynton */}
+        <div style={{ flex: 1, border: '1px solid #ccc', padding: '1rem' }}>
           <h1>Lynton</h1>
-          
           {/* <Button onClick={handleUploadtoLynton}>Upload to Lynton</Button> */}
         </div>
+      </div>
     </div>
   )
 }
